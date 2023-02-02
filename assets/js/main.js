f6a55529161874173c6818c5671d36a4162c1869
oscaresteban0314 <oatenas78@gmail.com>
2023-02-01 23:05:44 -0500

    menu nav responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 const desktop_menu = document.querySelector('.desktop-menu');
 const mobile_menu = document.querySelector('.menu_logo');
 const nav_menu = document.querySelector('.mobile-menu');
+const content = document.querySelector('.body');
 mobile_menu.addEventListener('click', toogledeskpotmenumobile);
-
 function toogledeskpotmenumobile() {
     nav_menu.classList.toggle('inactive');
+    if(nav_menu.classList.contains('inactive')==false) {
+        content.classList.toggle('inactive');
+    }
+    else{
+        content.classList.toggle('inactive');
+    }
 }
 
